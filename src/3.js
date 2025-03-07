1363ce7e71800ded6972f8a5c20081826dc072a7
borghiisabelle303 <borghiisabelle303@gmail.com>
2025-03-07 08:30:37 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,10 @@
+// Create a function to get a random number between 1 and 6
+function getRandomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+// Use the function to get a random number and store it in a variable
+const randomNumber = getRandomNumber();
+
+// Print the random number to the console
+console.log(randomNumber);
